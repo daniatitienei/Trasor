9b1ee48b71c7a750a5abc7b8ae56afebffb1687a
daniatitienei <daniatitienei@gmail.com>
2022-08-20 23:36:40 +0300

    Setting up dagger hilt

---
+++ app/src/main/java/com/atitienei_daniel/trasor/TrasorApplication.kt
@@ -0,0 +1,7 @@
+package com.atitienei_daniel.trasor
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrasorApplication : Application()
---
+++ feature-games/src/main/java/com/atitienei_daniel/feature_games/GamesViewModel.kt
@@ -0,0 +1,6 @@
+package com.atitienei_daniel.feature_games
+
+import androidx.lifecycle.ViewModel
+
+class GamesViewModel : ViewModel() {
+}
