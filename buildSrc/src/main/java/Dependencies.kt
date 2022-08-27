cffbd1d3f451f9be0a2997b4f9b3c61303b6bfe5
Daniel Atitienei <daniatitienei@gmail.com>
2022-08-27 12:21:37 +0300

    created buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,59 @@
+object Dependencies {
+    val navigationCompose by lazy {
+        "androidx.navigation:navigation-compose:${Versions.navigation}"
+    }
+    val lifecycleViewModelCompose by lazy {
+        "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.lifecycle}"
+    }
+    val lifecycleViewModelKtx by lazy {
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+    }
+    val lifecycleRuntimeCompose by lazy {
+        "androidx.lifecycle:lifecycle-runtime-compose:${Versions.lifecycle}"
+    }
+    val lifecycleRuntime by lazy {
+        "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntime}"
+    }
+    val hiltAndroid by lazy {
+        "com.google.dagger:hilt-android:${Versions.hilt}"
+    }
+    val hiltAndroidCompiler by lazy {
+        "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+    }
+    val hiltNavigationCompose by lazy {
+        "androidx.hilt:hilt-navigation-compose:${Versions.androidxHilt}"
+    }
+    val hiltCompiler by lazy {
+        "androidx.hilt:hilt-compiler:${Versions.androidxHilt}"
+    }
+    val composeUi by lazy {
+        "androidx.compose.ui:ui:${Versions.composeUi}"
+    }
+    val composeUiToolingPreview by lazy {
+        "androidx.compose.ui:ui-tooling-preview:${Versions.composeUi}"
+    }
+    val coreKtx by lazy {
+        "androidx.core:core-ktx:${Versions.core}"
+    }
+    val material3 by lazy {
+        "androidx.compose.material3:material3:${Versions.material3}"
+    }
+    val activityCompose by lazy {
+        "androidx.activity:activity-compose:${Versions.activity}"
+    }
+    val jUnit by lazy {
+        "junit:junit:${Versions.jUnit}"
+    }
+    val espressoCore by lazy {
+        "androidx.test.espresso:espresso-core:${Versions.espresso}}"
+    }
+    val composeUiTooling by lazy {
+        "androidx.compose.ui:ui-tooling:${Versions.composeUi}"
+    }
+    val composeUITestManifest by lazy {
+        "androidx.compose.ui:ui-tooling:${Versions.composeUi}"
+    }
+    val composeUiTestJUnit by lazy {
+        "androidx.compose.ui:ui-test-junit4:${Versions.composeUi}"
+    }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,17 @@
+object Versions {
+    const val material3 = "androidx.compose.material3:material3:1.0.0-beta01"
+    const val compose = "1.3.0"
+    const val composeUi = "1.2.1"
+    const val room = "2.4.3"
+    const val hilt = "2.42"
+    const val lifecycle = "2.6.0-alpha01"
+    const val accompanist = "0.26.1-alpha"
+    const val jUnit = "4.13.2"
+    const val androidxHilt = "1.0.0"
+    const val navigation = "2.5.1"
+    const val uiDebug = "1.2.1"
+    const val activity = "1.5.1"
+    const val lifecycleRuntime = "2.5.1"
+    const val core = "1.8.0"
+    const val espresso = "3.4.0"
+}
