32042a436c8def5f74394d7f593aca1b959d1776
Daniel Atitienei <daniatitienei@gmail.com>
2022-08-27 23:22:56 +0300

    migrated core-ui build.gradle to kotlin

---
+++ core-ui/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.atitienei_daniel.core_ui"
+    compileSdk = ConfigData.compileSdkVersion
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.iconsExtended)
+    implementation(Dependencies.googleFonts)
+
+    implementation(Dependencies.composeUi)
+    implementation(Dependencies.composeUiToolingPreview)
+    implementation(Dependencies.composeUiTooling)
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.material3)
+    implementation(Dependencies.lifecycleRuntimeKtx)
+    implementation(Dependencies.activityCompose)
+    debugImplementation(Dependencies.composeUiTestManifest)
+}
